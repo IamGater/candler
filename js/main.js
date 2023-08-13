9e69670eea713ed4f8027fa7fa83e9d865aa0468
IamGater <116513307+IamGater@users.noreply.github.com>
2023-08-13 17:43:27 +0300

    feedback

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ const headerList = document.querySelector('.header__list')
 burger.addEventListener('click', () => {
   headerList.classList.toggle('burger__active')
 })
+
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+
+  // pagination: {
+  //   el: '.swiper-pagination',
+  // },
+});
+const swiperTwo = new Swiper('.certificate__slider', {
+  loop: true,
+  slidesPrewView: 2,
+  spaceBetween: 20,
+  
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
