be7c0ae9a09ccc58ca6227b4f27ee8456184ea48
IamGater <116513307+IamGater@users.noreply.github.com>
2023-08-16 10:37:13 +0300

    acordeon

--- js/main.js
+++ js/main.js
@@ -22,3 +22,24 @@ const swiperTwo = new Swiper('.certificate__slider', {
     el: '.swiper-pagination',
   },
 });
+
+const acordeon = document.querySelector('.acordeon');
+
+const acordeonTitles = acordeon.querySelectorAll('.acordeon__title');
+
+acordeonTitles.forEach.call(acordeonTitles, function (acordeonTitle) {
+  acordeonTitle.addEventListener('click', function () {
+
+    const currentText = acordeonTitle.parentElement.querySelector('acordeon__text');
+
+    acordeonTitle.classList.toggle('acordeon__title-active');
+
+    acordeonText.classList.toggle('acordeon__text-active');
+
+    if(currentText.classList.contains('acordeon__text-active')){
+      currentText.style.maxHeight = currentText.scrollHeight + 'px'
+    } else {
+      currentText.style.maxHeight = null;
+      }
+  })
+});
